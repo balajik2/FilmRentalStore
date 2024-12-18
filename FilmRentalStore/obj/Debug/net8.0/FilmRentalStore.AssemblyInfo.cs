49cc6536a93693c2ffd9afe1dcdeb41be473b7d3
SHAMITAA <shamitaa.shamitaa@capgemini.com>
2024-12-18 16:00:50 +0530

    SQL Connection

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb9f225075de96c823dd210c14db472f2bcade0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980b227cfbe5c481e5a10669e09f5a0a9f2f70be")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
