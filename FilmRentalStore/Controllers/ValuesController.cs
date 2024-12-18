1c2adb354a8eeb6f4ceaa1c91707ee7a8fd107bc
Balaji Kumaresan <balaji.kumaresan@capgemini.com>
2024-12-18 12:58:16 +0530

    Initial Commit

---
+++ FilmRentalStore/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmRentalStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
