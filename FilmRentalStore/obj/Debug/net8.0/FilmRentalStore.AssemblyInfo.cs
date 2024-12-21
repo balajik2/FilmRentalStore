c0c1ed7fb9bcbba8ac6423406c5a2a8403ed8184
GOKULRAJ K <gokulraj.k@capgemini.com>
2024-12-21 13:30:30 +0530

    Stage Commit

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41183e24c429fb30482f90eb53b76eb2010e9c66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+028bcb8090b13f3eb9dd77ad7c2066ba822dc88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
