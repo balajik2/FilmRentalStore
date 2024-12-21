06ee8193a8504a3baf5ff3e35890aa1cdcff9b88
Balaji Kumaresan <balaji.kumaresan@capgemini.com>
2024-12-21 12:10:17 +0530

    Commit All

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc89ad2e2490e5f3f99d3419e6e7f8ea8ef5976b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e383fa50265ca06899fdac7837b1627c55ed66e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
