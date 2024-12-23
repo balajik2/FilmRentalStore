465a7e90341cd6da763a400b03bf2766cd4a86d9
GOKULRAJ K <gokulraj.k@capgemini.com>
2024-12-23 12:16:14 +0530

    Commit All

---
+++ FilmRentalStore/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilmRentalStore.Models;
+
+public partial class Role
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Ruser> Rusers { get; set; } = new List<Ruser>();
+
+    public virtual ICollection<Staff> Staff { get; set; } = new List<Staff>();
+}
