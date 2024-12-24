2592ac969e2e720d1e050fbdbc2f820cf3cccb10
Swetha V <swetha.e.v@capgemini.com>
2024-12-24 15:38:38 +0530

    commit staged

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e57353def59a22945a39302794608855ea6673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb9f225075de96c823dd210c14db472f2bcade0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
