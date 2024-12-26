2a1cfea249f4a02b1172e542f6fce9889b371fb2
SHAMITAA <shamitaa.shamitaa@capgemini.com>
2024-12-26 10:28:37 +0530

    Commit Staged

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8614ee8a8112e1c1d40618270ad73d382634af68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf91095f3b32b76a275dd355be1751fca6a334f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
