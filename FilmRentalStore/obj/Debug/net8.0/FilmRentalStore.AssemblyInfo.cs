84d41cc7ca3867409be43ebc1e765c7c9c3d5040
Basamkavya <kavya.basam@capgemini.com>
2024-12-26 10:34:40 +0530

    Commit All

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d159a8b2b6a4dda92ec965363c98b28ac82d95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc089459b2fa098e03c350e547a91a57b4463ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
