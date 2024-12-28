f7f8da4c384f24140a7a1c18ffe62017e4bbbedb
GOKULRAJ K <gokulraj.k@capgemini.com>
2024-12-28 19:20:08 +0530

    commit all

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15323dd5a056da9bcb273c0599abbe13b95a591b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8dde0fe6c37d9c0ed4537234840529752a5deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
