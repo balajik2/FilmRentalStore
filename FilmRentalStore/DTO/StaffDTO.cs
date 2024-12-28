9131a4e981a08f5d5854833e9f88b9a1bd3a7f13
GOKULRAJ K <gokulraj.k@capgemini.com>
2024-12-28 19:06:29 +0530

    Commit All

--- FilmRentalStore/DTO/StaffDTO.cs
+++ FilmRentalStore/DTO/StaffDTO.cs
@@ -16,9 +16,7 @@
 
             public bool Active { get; set; }
 
-            public string Username { get; set; } = null!;
-
-            public string? Password { get; set; }
+           
 
             public DateTime LastUpdate { get; set; }
 
--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee15df09de3379cb1c127a95c593d6cb99fd6ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51433fb7036c9dfa97482c778a08998174513615")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
