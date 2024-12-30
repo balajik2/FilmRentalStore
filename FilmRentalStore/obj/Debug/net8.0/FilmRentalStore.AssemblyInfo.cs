289d79ee055d2818981880749f37aacdc22bead1
Balaji Kumaresan <balaji.kumaresan@capgemini.com>
2024-12-30 21:38:04 +0530

    Configure cdn Image

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0222199cf702772d485f03c5d7ee625f1bc8cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee4d888526b6c05b3a2d43c3eb57024786f20a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
