3d23bf073805293933ec40d33bec156e1f52ad31
Balaji Kumaresan <balaji.kumaresan@capgemini.com>
2024-12-30 22:13:34 +0530

    Commit after Remove HEAD

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0222199cf702772d485f03c5d7ee625f1bc8cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef1949ef92ebcc8a0881ea4796cae34f479e427")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
