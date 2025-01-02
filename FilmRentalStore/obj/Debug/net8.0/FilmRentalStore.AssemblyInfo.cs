c9b6a63eccf04649c57e67b0037ef9f77d660751
SHAMITAA <shamitaa.shamitaa@capgemini.com>
2025-01-02 12:06:57 +0530

    pull

--- FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
+++ FilmRentalStore/obj/Debug/net8.0/FilmRentalStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed82b79ebdffddaa668006501b788df01d60798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12eb54cbed11bf15164a33c0ca6a2023f99b2127")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRentalStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
